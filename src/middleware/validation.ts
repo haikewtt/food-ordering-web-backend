a28a2206a689b22a26cdfafaa65e05e0ac66ea6a
haikewtt <haikewtt@gmail.com>
2024-10-01 02:44:58 +0700

    adding user profile feature

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -17,3 +17,16 @@ export const validateMyUserRequest = [
     body("country").isString().notEmpty().withMessage("Country must be a string"),
     handleValidationErrors,
 ];
+
+export const validateMyRestaurantRequest = [
+    body("restaurantName").notEmpty().withMessage("Restaurant name is required"),
+    body("city").notEmpty().withMessage("City name is required"),
+    body("country").notEmpty().withMessage("Country name is required"),
+    body("deliveryPrice").isFloat({min: 0}).withMessage("Delivery price must be positive"),
+    body("estimatedDeliveryTime").isInt({min: 0}).withMessage("Estimated time must be posivite"),
+    body("cuisines").isArray().withMessage("Cuisines must be an array").not().isEmpty().withMessage("Cuisines array can not be empty"),
+    body("menuItems").isArray().withMessage("Menu item must be an array"),
+    body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+    body("menuItems.*.price").isFloat({min: 0}).withMessage("Menu item price is required and positive"),
+    handleValidationErrors, 
+];
