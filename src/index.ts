ad988314544a4f720251329298e3da0288eddffa
haikewtt <haikewtt@gmail.com>
2024-10-13 19:22:03 +0700

    adding checkout flow

--- src/index.ts
+++ src/index.ts
@@ -3,35 +3,38 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/myUserRoute";
-import { v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 
-
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>{
-    console.log("connected");
-});
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"));
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
-app.use(express.json());
+
 app.use(cors());
 
-app.get("/health", async (req: Request, res: Response)=> {
-    res.send({message: "health ok!"});
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json());
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
 });
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
 
-
-app.listen(7000, ()=>{
-    console.log("server start on localhost:7000");
+app.listen(7000, () => {
+  console.log("server started on localhost:7000");
 });
-
